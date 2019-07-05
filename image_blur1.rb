89d5ae907f97a00390730e393c052ec14d5799b1
Rosa <lealeon@att.net>
2019-07-05 23:28:51 +0000

    blur three

--- image_blur1.rb
+++ image_blur1.rb
@@ -1,35 +1,56 @@
 class Image
-  attr_accessor :image 
+  attr_accessor :image
 
-def initialize (image)
-  self.image = image
-end
-
-def blur
-  location = []
-  image.each_with_index do |row, i|
-    row.each_with_index do |x, row_i|
-    if x == 1
-      location << [i, row_i]
-       puts "#{i} and #{row_i}"
-     end
-    end
+  def initialize(image)
+    self.image = image
   end
-
 
   def output_image
     image.each do |row|
       puts row.join
     end
   end
+
+  def blurred
+    location = []
+    image.each_with_index do |row,i|
+      row.each_with_index do |x, row_i|
+      if x == 1
+        location << [i, row_i]
+      #puts "#{i} and #{row_i}"
+      #puts "#{location}"
+      end
+      end  
+    end
+
+    #blurred_image = []
+    location.each do |i, row_i|
+      image[i-1][row_i] = 1 
+      image[i+1][row_i] = 1
+      image[i][row_i-1] = 1
+      image[i][row_i+1] = 0
+      #blurred_image<<
+    #puts image.join
+    end  
+  end
+
+  
+
+  def blur(distance)
+    distance.times do
+      blurred
+    end
+  end
+    
 end
 
 image = Image.new([
-  [0, 0, 0, 0],
-  [0, 1, 0, 0],
-  [0, 0, 0, 1],
-  [0, 0, 0, 0]
+  [0, 0, 0, 0, 0],
+  [0, 1, 0, 0, 0],
+  [0, 0, 0, 0, 0],
+  [0, 0, 0, 1, 0],
+  [0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0],
 ])
-image.blur
- 
- end     
+image.blur(2)
+image.output_image
