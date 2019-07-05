555da99062c47f70b3aab201e8c417b3eb73747f
Rosa <lealeon@att.net>
2019-07-05 20:15:32 +0000

    blur two

--- image_blur1.rb
+++ image_blur1.rb
@@ -5,17 +5,31 @@ def initialize (image)
   self.image = image
 end
 
-def output_image
-  @image.each do |data|
-puts data.join
+def blur
+  location = []
+  image.each_with_index do |row, i|
+    row.each_with_index do |x, row_i|
+    if x == 1
+      location << [i, row_i]
+       puts "#{i} and #{row_i}"
+     end
+    end
+  end
+
+
+  def output_image
+    image.each do |row|
+      puts row.join
+    end
+  end
 end
-end
-end
+
 image = Image.new([
   [0, 0, 0, 0],
   [0, 1, 0, 0],
   [0, 0, 0, 1],
   [0, 0, 0, 0]
 ])
-image.output_image
-      
+image.blur
+ 
+ end     
